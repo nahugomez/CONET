90b35ced7785b9b7f6e2f530cf9b52bafac63bd7
Nahuel Gomez <nahuelgomezs@outlook.com>
2022-04-06 19:12:13 -0300

    V1.0

---
+++ README.md
@@ -0,0 +1,12 @@
+# CONET 
+### Desarrollo de Webs económicas y de calidad en San Juan
+
+* * *
+
+*V1.0*:
+- FUNCIONALIDAD (LISTO)
+- SCROLLREVEAL (LISTO)
+- RESPONSIVE DESIGN (LISTO)
+- HTML, CSS y JS (LISTO)
+- TEXTO INCLUIDO 
+
---
+++ js/main.js
@@ -0,0 +1,58 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.navbar-brand', {
+    duration: 3000,
+    origin: 'top',
+    distance: '100px',
+});
+
+sr.reveal('.navbar-toggler-icon', {
+    duration: 3000,
+    origin: 'top',
+    distance: '100px',
+});
+
+sr.reveal('.navbar-toggler', {
+    duration: 3000,
+    origin: 'top',
+    distance: '100px',
+});
+
+sr.reveal('.info', {
+    duration: 3000,
+});
+
+sr.reveal('.header', {
+    duration: 3000,
+    origin: 'left',
+    distance: '300px',
+});
+
+sr.reveal('.cards', {
+    duration: 3000,
+});
+
+sr.reveal('.ad', {
+    duration: 3000,
+});
+
+sr.reveal('.services', {
+    duration: 3000,
+    origin: 'left',
+    distance: '300px',
+});
+
+sr.reveal('.aboutUs', {
+    duration: 3000,
+});
+
+sr.reveal('.contact', {
+    duration: 3000,
+    origin: 'left',
+    distance: '300px',
+});
+
+
+
+
+
